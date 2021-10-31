2bc34e50c40d1a4aac34c38d899d38d5ec6d5f21
GrouchyDwarf <ivanyeremenko3@gmail.com>
2021-10-31 17:26:16 +0200

    created project and added db creation script

---
+++ Api/db_creation.sql
@@ -0,0 +1,27 @@
+﻿drop database if exists FlashcardsDb;
+go
+create database FlashcardsDb;
+go
+use FlashcardsDb;
+
+create table Decks(
+    Id bigint not null identity(1, 1) primary key,
+    Name varchar(30) not null
+);
+
+alter table Decks
+add constraint UC_Decks unique(Name);
+
+create table Flashcards(
+    Id bigint not null identity(1, 1) primary key,
+    Phrase varchar(50) not null,
+    Translation varchar(50) not null, 
+    DeckId bigint not null,
+    WasMemorized bit not null
+);
+
+alter table Flashcards
+add constraint UC_Flashcards unique(Phrase);
+
+alter table Flashcards
+add constraint FK_Flashcards FOREIGN KEY(DeckId) REFERENCES Decks(Id);
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Configuring Api
+1. Run `db_creation.sql` in `Api` folder
+2. Create `appsettings.json` according to `appsettings.example.json` in `Api` folder
